1e09219bd9f6fd240735fbdf1590d99c6e9add0f
AwesomeDevin <awesomedevin.deng@gmail.com>
2024-02-08 18:09:46 +0800

    commit

--- packages/vergo/config/constant.ts
+++
@@ -1,4 +0,0 @@
-
-
-export const DEFAULT_REGISTRY = process.env.REGISTRY || 'https://registry.npmjs.org/'
-export const DEFAULT_IS_BETA = process.env.BETA === 'true' || false
--- packages/vergo/src/log.ts
+++
@@ -1,4 +0,0 @@
-import { Logger } from '../../../utils'
-
-const name = 'vergo'
-export const asvCliLogger = new Logger(name)
